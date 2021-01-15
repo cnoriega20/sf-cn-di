f4a22be044f6a8ee86a886a744f4ff2bc2a84570
cnoriega20 <c79.noriega@gmail.com>
2021-01-15 00:05:54 -0600

    Added Another property file. Added configuration.

--- src/main/java/com/sprinboot/example/springbootdidemo/SpringbootDiDemoApplication.java
+++ src/main/java/com/sprinboot/example/springbootdidemo/SpringbootDiDemoApplication.java
@@ -1,10 +1,7 @@
 package com.sprinboot.example.springbootdidemo;
 
-import com.sprinboot.example.springbootdidemo.controllers.ConstructorInjectedController;
-import com.sprinboot.example.springbootdidemo.controllers.GetterInjectedController;
-import com.sprinboot.example.springbootdidemo.controllers.HelloController;
-import com.sprinboot.example.springbootdidemo.controllers.PropertyInjectedController;
 import com.sprinboot.example.springbootdidemo.externalization.DummyDatasource;
+import com.sprinboot.example.springbootdidemo.externalization.DummyJmsBroker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -16,10 +13,17 @@ public class SpringbootDiDemoApplication {
 
         ApplicationContext ctx = SpringApplication.run(SpringbootDiDemoApplication.class, args);
         DummyDatasource dummyDatasource = (DummyDatasource)ctx.getBean(DummyDatasource.class);
+        DummyJmsBroker dummyJmsBroker = (DummyJmsBroker)ctx.getBean(DummyJmsBroker.class);
 
+        System.out.println("Printing from datasource.properties...");
         System.out.println(dummyDatasource.getUsername());
         System.out.println(dummyDatasource.getPassword());
         System.out.println(dummyDatasource.getUrl());
+
+        System.out.println("Printing from jms.properties...");
+        System.out.println(dummyJmsBroker.getUsername());
+        System.out.println(dummyJmsBroker.getPassword());
+        System.out.println(dummyJmsBroker.getUrl());
     }
 
 }
--- src/main/java/com/sprinboot/example/springbootdidemo/config/PropertyConfig.java
+++ src/main/java/com/sprinboot/example/springbootdidemo/config/PropertyConfig.java
@@ -1,14 +1,19 @@
 package com.sprinboot.example.springbootdidemo.config;
 
 import com.sprinboot.example.springbootdidemo.externalization.DummyDatasource;
+import com.sprinboot.example.springbootdidemo.externalization.DummyJmsBroker;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 @Configuration
-@PropertySource("classpath:datasource.properties")
+@PropertySources({
+        @PropertySource("classpath:datasource.properties"),
+        @PropertySource("classpath:jms.properties")
+})
 public class PropertyConfig {
     @Value("${cn.username}")
     String username;
@@ -19,6 +24,15 @@ public class PropertyConfig {
     @Value("${cn.githuburl}")
     String url;
 
+    @Value("${cn.jms.username}")
+    String jmsUsername;
+
+    @Value("${cn.jms.password}")
+    String jmsPassword;
+
+    @Value("${cn.jms.githuburl}")
+    String jmsUrl;
+
     @Bean
     public DummyDatasource dummyDatasource(){
         DummyDatasource dummyDatasource = new DummyDatasource();
@@ -26,6 +40,15 @@ public class PropertyConfig {
         dummyDatasource.setPassword(password);
         dummyDatasource.setUrl(url);
         return dummyDatasource;
+    }
+
+    @Bean
+    public DummyJmsBroker dummyJmsBroker(){
+        DummyJmsBroker dummyJmsBroker = new DummyJmsBroker();
+        dummyJmsBroker.setUsername(jmsUsername);
+        dummyJmsBroker.setPassword(jmsPassword);
+        dummyJmsBroker.setUrl(jmsUrl);
+        return dummyJmsBroker;
     }
 
     @Bean
